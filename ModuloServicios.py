bab6088433f4ecb73d133b1852a80fa307f537ee
JairGarcia <156432366+JairBGarcia@users.noreply.github.com>
2024-02-16 09:53:28 -0500

    aplazao

--- ModuloServicios.py
+++ ModuloServicios.py
@@ -2,22 +2,29 @@ import os
 import json
 
 
+def validarIden(identidad):
+    if identidad == "":
+        return False
+    try:
+        identidad = int(identidad) 
+        if identidad > 0:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
 
 def inscripciones_servicios():
     
     ruta = os.path.join(os.path.dirname(__file__), 'InscritosCampers.json')
     with open(ruta,'r') as archivo:
         datos_json = json.load(archivo)
-    
+    estado = 'en venta'
     nuevo_servicio = {}
     identidad = input('Ingresa el número de identidad del usuario: ')
     nuevo_servicio['identidad'] = int(identidad)
     nuevo_servicio['nombre'] = input('Ingresa el nombre del servicio: ')
-    nuevo_servicio['apellido1'] = input('Ingrese el primer apellido del usuario: ')
-    nuevo_servicio['direccion'] = input('Ingrese la direccion del usuario: ')
-    nuevo_servicio['edad'] = int(input('Ingrese la edad: '))
-    nuevo_servicio['celular'] = int(input('Ingrese el numero de telefono '))
-    
+    nuevo_servicio['estado'] = estado
     
     datos_json['datos']['servicios'].append(nuevo_servicio)
     with open(ruta,'w') as archivo:
@@ -29,59 +36,39 @@ def actualizar_servicios():
     with open(ruta,'r') as archivo:
         datos_json = json.load(archivo)
     campers = datos_json["datos"]["servicios"]
-    identidad = input("Ingresa el id del Camper que quieras actualizar: ")
+    identidad = input("Ingresa el serial del servicio: ")
     try:
         identidad = int(identidad)
     except ValueError:
-        print('El id del Camper debe ser un número válido.')
+        print('El serial debe ser valido.')
     for camper in campers:
         if camper['identidad'] == identidad:
             preguntas = {
-                'identidad': ('Ingresa el número de identidad del ususario: ', validarIden),
-                'nombre': ('Ingresa el nombre del usuario: ', None),
-                'apellido1': ('Ingrese el primer apellido del usuario: ', None),
-                'direccion': ('Ingrese la direccion del usuario: ', None),
-                'edad': ('Ingrese la edad del usuario: ', validarEdad),
-                'celular': ('Ingresa el número de celular del usuario: ', validarIden),
+                'identidad': ('Ingresa el serial del servicio: ', validarIden),
+                'nombre': ('Ingresa el nombre del servicio: ', None),
                 'estado': ('Ingrese el estado del usuario: ', None)
             }
-            for clave, valor in preguntas.items():
-                mensaje, validacion = valor
-                nuevo_dato = input(mensaje)
-                if validacion:
-                    while not validacion(nuevo_dato):
-                        print(f'El {clave} debe ser válido.')
-                        nuevo_dato = input(mensaje)
-                if clave in ['identidad', 'edad', 'celular_acudiente', 'celular', 'telefono']:
-                    nuevo_dato = int(nuevo_dato)
-                camper[clave] = nuevo_dato
-            print(f'Se ha actualizado el camper con id {identidad}')
-            break
     else:
-        print(f'No se encontró ningún camper con id {identidad}')
+        print(f'No se encontró ningún serial {identidad}')
     with open(ruta, 'w') as archivo:
         json.dump(datos_json, archivo, indent=4)
 
 
 def buscar_servicios():
-    identidad = input("Ingresa el id del Camper que quieras buscar: ")
+    identidad = input("Ingresa el serial de servicio: ")
     try:
         identidad = int(identidad)
     except ValueError:
-        print('El id del Camper debe ser un número válido.')
+        print('El serial no es correcto.')
     return identidad
 
 def mostrar_servicios(ruta):
   with open(ruta, 'r') as archivo:
     datos_json = json.load(archivo)
-  usuario = datos_json["datos"]["usuarios"]
+  usuario = datos_json["datos"]["servicios"]
   for usuarios in usuario:
     print(f"Id: {usuarios['identidad']}")
     print(f"Nombre: {usuarios['nombre']}")
-    print(f"Apellido 1: {usuarios['apellido1']}")
-    print(f"Dirección: {usuarios['direccion']}")
-    print(f"Edad: {usuarios['edad']}")
-    print(f"Celular: {usuarios['celular']}")
     print()
     
 #inscripciones()
